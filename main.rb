bffc03311d0c8159de3903ddfc50106b7ccf55aa
Jerry Bach <125259632+jerrybach98@users.noreply.github.com>
2023-09-01 20:39:13 -0700

    correct logic to display clues and handle invalid inputs
    A 4-digit code is required to break a loop that asks for the codebreaker guess

--- main.rb
+++ main.rb
@@ -1,50 +1,47 @@
-# Build the game assuming the computer randomly selects the secret colors and the human player must guess them. Remember that you need to give the proper feedback on how good the guess was each turn!
 # Use instance variables for state of game (turn number, secret code, feedback, names, configurable settings)
 # local: Current guess, winner, code for evaluation, valid_input, etc
 
 # Brainstorm:
 #compare 1 dimensional code arrays to get guess array to indicate clues on each loop
-# Do a 12 round game loop
-# Take guess on each round, award hint for correct position
-# if number matches number in array award +1
-
-# 1 2 1 3
-# 1 5 3 1 [⚫ ○ ○]
-# Gives 1 number match and 1 direct match
-# number match = number match - direct match
-# 1 number in the wrong place, 1 number in the right place
 
 
-# clues: add solid clue for correct position and guess, and empty circle for correct guess but not right position 
+
+# Logic to handle invalid inputs
 # Class for game, codemaker, codebreaker
 # Explain rules in console before playing
-#
+# Do a 12 round game loop
 
 class Mastermind
   def initialize (computer, player)
-    puts "Welcome to Mastermind. The object of Mastermind is to guess a secret code consisting of a series of 4 numbers between 1-6 with duplicates allowed. Each guest results in feedback narrowing down the possibilities of the code. The codebreaker tries to guess the pattern in order within twelve turns. The feedback will indicate whether a guess is correct in both number and position as (⚫) or indicate the existence of a correct code placed in the wrong position as (○).
+    puts "Welcome to Mastermind. The object of Mastermind is to guess a secret code consisting of a series of 4 numbers between 1-6 with duplicates allowed. Each guess results in feedback narrowing down the possibilities of the code. The codebreaker tries to guess the pattern in order within twelve turns. The feedback will indicate whether a guess is correct in both number and position as (●) or indicate the existence of a correct code placed in the wrong position as (○).
     "
     @computer = computer
     @player = player
   end
 
   def play
-    #p computer_code = @computer.generate_code
-    p computer_code = [1, 2, 1, 3]
+    p computer_code = @computer.generate_code
+    #p computer_code = [1, 1, 2, 2] 
     p guess = @player.make_guess
      compare_guess(computer_code, guess)
   end 
 
   def compare_guess(computer_code, guess)
     feedback_array = []
-    positional_match = guess.map.with_index { |e,i| e == computer_code[i] }
+    # Returns true in match positions and counts number of trues in array
+    positional_match = guess.map.with_index { |e, i| e == computer_code[i] }
     match_number = positional_match.count(true)
-    feedback_array.fill("⚫", feedback_array.size, match_number)
+    feedback_array.fill("● ", feedback_array.size, match_number)
 
-    #logic for same number but not exact position
-    exist_count = computer_code.count{|e| guess.include?(e)} - match_number
-    feedback_array.fill("○", feedback_array.size, exist_count)
+    # Remove positional matches from array
+    modified_guess_array = guess.reject.with_index { |e, i| positional_match[i] }
+    modified_code_array = computer_code.reject.with_index { |e, i| positional_match[i] }
+
+    #Compares modified array
+    exist_count = modified_guess_array.count{|e| modified_code_array.include?(e)}
+    feedback_array.fill("○ ", feedback_array.size, exist_count)
     puts "Clues: #{feedback_array.join}"
+    p feedback_array
 
   end
 
@@ -58,12 +55,20 @@ class Computer
 end
 
 class Player
-  def initialize
-  end
+  
   def make_guess
-    guess_code = gets.chomp
-    substrings = guess_code.split(//).map(&:to_i)
+    loop do
+      guess_code = gets.chomp
+      substrings = guess_code.split(//).map(&:to_i)
+      check = substrings.all? { |num| num.between?(1, 6)}
+        if guess_code.length != 4 || check == false
+          puts "Invalid input. Please enter a 4-digit code with digits between 1 and 6"
+          next
+        end
+      break substrings
+    end
   end
+
 end
 
 class GameBoard
